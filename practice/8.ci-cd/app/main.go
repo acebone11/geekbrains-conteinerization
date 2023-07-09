32aeb7895e20b7cff0bcf88adf703ef5bd518b11
acebone11 <91693046+acebone11@users.noreply.github.com>
2023-07-09 13:33:53 +0300

    Update main.go

--- practice/8.ci-cd/app/main.go
+++ practice/8.ci-cd/app/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/pauljamm/geekbrains-conteinerization/practice/8.ci-cd/app/app"
-	"github.com/pauljamm/geekbrains-conteinerization/practice/8.ci-cd/app/config"
+	"github.com/acebone11/geekbrains-conteinerization/practice/8.ci-cd/app/app"
+	"github.com/acebone11/geekbrains-conteinerization/practice/8.ci-cd/app/config"
 )
 
 func main() {
